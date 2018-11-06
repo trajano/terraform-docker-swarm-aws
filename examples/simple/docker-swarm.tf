bacb7285205ff9eb9923e701b15d7bc96168f37f
Archimedes Trajano <developer@trajano.net>
2018-11-06 12:43:16 -0500

    Added example

---
+++ examples/simple/docker-swarm.tf
@@ -0,0 +1,11 @@
+module "docker-swarm" {
+  source  = "trajano/swarm-aws/docker"
+  version = "1.0.3"
+  
+  name   = "My VPC Swarm"
+  vpc_id = "${aws_vpc.main.id}"
+  cloud_config_extra = "${file("users.cloud-config")}"
+  exposed_security_group_ids = [
+    "${aws_security_group.exposed.id}",
+  ]
+}
