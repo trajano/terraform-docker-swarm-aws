f768c12b0d831a687a59cec7223550d31db1e815
Archimedes Trajano <developer@trajano.net>
2019-07-11 23:41:01 -0400

    ran terraform 0.12upgrade

--- workers.tf
+++ workers.tf
@@ -1,75 +1,86 @@
 data "template_file" "init_worker" {
-  count    = "${var.workers}"
-  template = "${file("${path.module}/init_worker.py")}"
+  count    = var.workers
+  template = file("${path.module}/init_worker.py")
 
-  vars {
-    s3_bucket      = "${aws_s3_bucket.terraform.bucket}"
-    instance_index = "${count.index}"
-    swapsize       = "${var.swap_size}"
-    vpc_name       = "${local.dns_name}"
+  vars = {
+    s3_bucket      = aws_s3_bucket.terraform.bucket
+    instance_index = count.index
+    swapsize       = var.swap_size
+    vpc_name       = local.dns_name
   }
 }
 
 data "template_cloudinit_config" "workers" {
-  count         = "${var.workers}"
+  count         = var.workers
   gzip          = "true"
   base64_encode = "true"
 
   part {
-    content = "${file("${path.module}/common.cloud-config")}"
+    content = file("${path.module}/common.cloud-config")
   }
 
   part {
     filename     = "extra.cloud-config"
-    content      = "${var.cloud_config_extra}"
+    content      = var.cloud_config_extra
     content_type = "text/cloud-config"
   }
 
   part {
     filename     = "init_worker.py"
-    content      = "${data.template_file.init_worker.*.rendered[count.index]}"
+    content      = data.template_file.init_worker[count.index].rendered
     content_type = "text/x-shellscript"
   }
 
   part {
     filename     = "extra.sh"
-    content      = "${var.cloud_config_extra_script}"
+    content      = var.cloud_config_extra_script
     content_type = "text/x-shellscript"
   }
 }
 
 resource "aws_instance" "workers" {
   depends_on = [
-    "aws_s3_bucket.terraform",
-    "aws_instance.managers",
+    aws_s3_bucket.terraform,
+    aws_instance.managers,
   ]
 
-  count         = "${var.workers}"
-  ami           = "${data.aws_ami.base_ami.id}"
-  instance_type = "${var.instance_type}"
-  subnet_id     = "${aws_subnet.workers.*.id[(count.index + var.managers) % length(data.aws_availability_zones.azs.*.names)]}"
-  private_ip    = "${cidrhost(aws_subnet.workers.*.cidr_block[count.index % length(data.aws_availability_zones.azs.*.names)], 10 + count.index)}"
+  count         = var.workers
+  ami           = data.aws_ami.base_ami.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.workers[count.index + var.managers % length(data.aws_availability_zones.azs.*.names)].id
+  private_ip = cidrhost(
+    aws_subnet.workers[count.index % length(data.aws_availability_zones.azs.*.names)].cidr_block,
+    10 + count.index,
+  )
 
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
   vpc_security_group_ids = [
-    "${local.security_group_ids}",
+    local.security_group_ids,
   ]
 
-  iam_instance_profile = "${aws_iam_instance_profile.ec2.name}"
-  user_data_base64     = "${data.template_cloudinit_config.workers.*.rendered[count.index]}"
-  key_name             = "${var.key_name}"
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
+  user_data_base64     = data.template_cloudinit_config.workers[count.index].rendered
+  key_name             = var.key_name
 
-  tags {
+  tags = {
     Name = "${var.name} worker ${count.index}"
   }
 
   root_block_device {
-    volume_size = "${var.volume_size}"
+    volume_size = var.volume_size
   }
 
   lifecycle {
     ignore_changes = [
-      "ami",
-      "user_data_base64",
+      ami,
+      user_data_base64,
     ]
   }
 
@@ -77,3 +88,4 @@ resource "aws_instance" "workers" {
     cpu_credits = "standard"
   }
 }
+
