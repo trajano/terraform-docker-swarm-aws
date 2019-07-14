9074d3e5a76082c4f0f5b2e8f288bea13fedd462
Archimedes Trajano <developer@trajano.net>
2019-07-13 21:05:42 -0400

    ran terraform 0.12upgrade

--- examples/simple/eips.tf
+++ examples/simple/eips.tf
@@ -1,9 +1,10 @@
 resource "aws_eip" "managers" {
   count    = "2"
-  instance = "${module.docker-swarm.manager_instance_ids[count.index]}"
+  instance = module.docker-swarm.manager_instance_ids[count.index]
   vpc      = true
 }
 
 output "manager_ip_addresses" {
-  value = "${aws_eip.managers.*.public_ip}"
+  value = aws_eip.managers.*.public_ip
 }
+
