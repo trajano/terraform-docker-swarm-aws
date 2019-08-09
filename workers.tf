fefd12d3acb819f6be9c85ca89b7ffc21ad8f115
Archimedes Trajano <developer@trajano.net>
2019-08-09 01:33:14 -0400

    Removed TODO

--- workers.tf
+++ workers.tf
@@ -53,17 +53,7 @@ resource "aws_instance" "workers" {
     10 + count.index,
   )
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  vpc_security_group_ids = [
-    local.security_group_ids,
-  ]
+  vpc_security_group_ids = local.security_group_ids
 
   iam_instance_profile = aws_iam_instance_profile.ec2.name
   user_data_base64     = data.template_cloudinit_config.workers[count.index].rendered
