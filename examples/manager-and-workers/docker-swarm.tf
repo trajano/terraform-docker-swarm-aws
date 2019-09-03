eee7c551b2240a1c829907c1d99b17388841c767
Yulei Liu <yulei.liu@gmail.com>
2019-09-03 19:55:11 +1000

    correct manager and worker counts
    manager and worker counts are wrong

--- examples/manager-and-workers/docker-swarm.tf
+++ examples/manager-and-workers/docker-swarm.tf
@@ -5,8 +5,8 @@ module "docker-swarm" {
 
   name               = "My VPC Swarm"
   vpc_id             = aws_vpc.main.id
-  workers            = 3
-  managers           = 5
+  workers            = 5
+  managers           = 3
   cloud_config_extra = file("users.cloud-config")
 
   exposed_security_group_ids = [
